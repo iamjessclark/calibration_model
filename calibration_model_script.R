c93279381cb4b465e4a5953df334fd9c8ca036f2
iamjessclark <jeclark1989@gmail.com>
2020-06-19 16:09:27 +0100

    model scripts and functions

---
+++ calibration_model_script.R
@@ -0,0 +1,86 @@
+# Calibrating schistosomiasis diagnostics
+# jessica clark 
+# June 2020 
+
+# packages #
+source("diagnositic.calibration.model.R")
+require(tidyverse)
+require(runjags)
+require(coda)
+
+# load data #
+
+kk.data <- read.csv("KK.4tp.csv") 
+cca.data <- read.csv("cca.clean.csv")
+
+# KK data for the model 
+kk <- loadKKdata("KK.4tp.csv")
+KKIDs <- getKKChildIDs("KK.4tp.csv")
+
+# cca data for the model 
+cca <- loadCCAdata("cca.clean.csv")
+CCAIDs <- getCCAChildIDs("cca.clean.csv")
+
+CID <- intersect(KKIDs, CCAIDs)
+kk <- kk[match(CID, KKIDs),,]
+cca <- cca[match(CID, CCAIDs),]
+
+# g score data for the model 
+
+ccagscore <- loadgscoredata("cca.clean.csv")
+gscoreCCAIDS <- getCCAChildIDsgscore("cca.clean.csv") 
+
+gscoreCID <- intersect(KKIDs,gscoreCCAIDS) 
+ccagscore <- ccagscore[match(CID,gscoreCID),]
+
+#### just KK model ####
+
+katokatz <- CalModKK(dim(kk)[1], dim(kk)[2], dim(kk)[3], kk)
+prevKK <- density(c(katokatz$mcmc[[1]][,"prev"], katokatz$mcmc[[2]][,"prev"]))
+model.output <- as.data.frame(as.matrix(as.mcmc(katokatz)))
+status <- model.output[,5:ncol(model.output)]
+status.time.KK <- time.steps(status)
+
+# This is a df for the geom_text with the co-ordinates of the labels 
+anno <- data.frame(c("Baseline", "ThreeWeeks", "NineWeeks", "SixMonths"))
+colnames(anno) <- "time"
+anno$xcoord <- 0
+anno$ycoord <-200
+anno$label <- as.factor(c("A", "B", "C", "D"))
+
+pdf("probinfKK.pdf",width=3,height=8)
+par(font=2, cex.axis=0.75, lwd=2, mar=c(2.1,2.2,1.2,0)+0.1,mgp=c(3,0.4,0))
+par(mfrow=c(1,1))
+
+prob.inf.kk <- qplot(value, data = status.time.KK, geom = "histogram", fill=time, binwidth=0.02) +
+  scale_fill_manual(values = c("#e7298a", "#1b9e77", "#d95f02", "#7570b3"))+
+  geom_text(data=anno, aes(x = xcoord, y = ycoord, label = label))+
+  facet_grid(time~.)+
+  theme_bw()+coord_cartesian(ylim=c(0, 200), xlim=c(0,1))+theme(legend.position = "none")+
+  ylab("Number of Hosts") + xlab("Probability of Being Infected")
+
+prob.inf.kk
+dev.off()
+
+#### KK and CCA Model ####
+
+kkcca.model <- CalModKKCCA(dim(kk)[1],dim(kk)[2],dim(kk)[3],kk,cca)
+
+prevKKcca <- density(c(kkcca.model$mcmc[[1]][,"prev"], kkcca.model$mcmc[[2]][,"prev"]))
+model.outputkkcca <- as.data.frame(as.matrix(as.mcmc(kkcca.model)))
+status.kkcca <- model.outputkkcca[,5:ncol(model.outputkkcca)]
+status.time.KKcca <- time.steps(status.kkcca)
+
+pdf("probinfKKcca.pdf",width=3,height=8)
+par(font=2, cex.axis=0.75, lwd=2, mar=c(2.1,2.2,1.2,0)+0.1,mgp=c(3,0.4,0))
+par(mfrow=c(1,1))
+
+prob.inf.kk <- qplot(value, data = status.time.KKcca, geom = "histogram", fill=time, binwidth=0.02) +
+  scale_fill_manual(values = c("#e7298a", "#1b9e77", "#d95f02", "#7570b3"))+
+  geom_text(data=anno, aes(x = xcoord, y = ycoord, label = label))+
+  facet_grid(time~.)+
+  theme_bw()+coord_cartesian(ylim=c(0, 200), xlim=c(0,1))+theme(legend.position = "none")+
+  ylab("Number of Hosts") + xlab("Probability of Being Infected")
+
+prob.inf.kk
+dev.off()
---
+++ diagnositic.calibration.model.R
@@ -0,0 +1,346 @@
+# Calibrating Schistosomiasis Diagnostics
+# Jessica Clark
+# Using the bayesian framework developed in the HMM to look at each time point independently
+# Status will not be tracked across time points but will be estimated independently
+
+# this is for just the KK data
+CalModKK <- function (N, Ti, R, KK) {
+  ## Set seed ##
+  .RNG.seed <- function(chain)
+    return( switch(chain, "1"= 1, "2"= 2) )
+  .RNG.name <- function(chain)
+    return( switch(chain, "1" = "base::Super-Duper", "2" = "base::Wichmann-Hill") )
+  
+  ## Initialize Status ##
+  Status <- matrix(1,nrow=210, ncol=4)
+  sigma <-  0.001
+  
+  m <- "model {
+    
+    # Prior prevalence  #
+
+    prev ~ dbeta(1,1)
+    
+    # Prior KKs #
+    
+    rtnb ~ dgamma(286.3235, 223.4650)
+    sh ~ dgamma(83.88592,125.70331)
+    rt1 ~ dbeta(47.13542,633.08366)
+    rt <- rt1/(1-rt1)
+    sigma ~ dgamma(0.001, 0.001)
+    tkksh ~ dgamma(0.001, 0.001)
+    tkkrt1 ~ dbeta(1,1)
+    tkkrt <- tkkrt1/(1-tkkrt1)
+    
+  # model 
+  for(n in 1:N){	# run through pop
+
+    for (t in 1:Ti){ # run through time
+      Status[n,t] ~ dbern(prev)
+      
+      tKK[n,1,t] <- 0
+      tKK[n,2,t] ~ dgamma(tkksh, tkkrt)
+      
+      lambda[n,t] <- tKK[n,Status[n,t]+1,t] 
+        
+        for(r in 1:R){ # run through repeated measures to set the baseline KK over the repeated measures, this is the LLH
+           KK[n,t,r] ~ dnegbin(rtnb/(lambda[n,t]+rtnb),rtnb)
+        } # end or r loop
+      } # end time
+  } # end n loop
+    
+    #inits# .RNG.seed, .RNG.name, Status, sigma
+    #data# N, Ti, R, KK
+    #monitor# prev, rtnb, sh, rt, Status, tKK
+}"
+  
+  
+  # Run model #
+  Results <- run.jags(m, burnin=5000, sample=10000, n.chains=2, jags.refresh = 1, method = 'parallel',
+                      plots = F, silent.jags = F)
+  return(Results)
+}
+
+#### CCA and KK ####
+
+CalModKKCCA <- function (N, Ti, R, KK, CCA) {
+  ## Set seed ##
+  ##this is so that they start on different values ##
+  .RNG.seed <- function(chain)
+    return( switch(chain, "1"= 1, "2"= 2) )
+  .RNG.name <- function(chain)
+    return( switch(chain, "1" = "base::Super-Duper", "2" = "base::Wichmann-Hill") )
+  
+  ## Initialize Status ##
+  Status <- matrix(1,nrow=210, ncol=4)
+  sigma <-  0.001
+  prob <- array(rep(CCA,2),dim=c(N,Ti,2))
+  
+  m <- "model {
+  # Prior prevalence / clearance / reinfection #
+  prev ~ dbeta(1,1)
+
+  # Prior KKs 
+  rtnb ~ dgamma(286.3235, 223.4650)
+  sh ~ dgamma(83.88592,125.70331)
+  rt1 ~ dbeta(47.13542,633.08366)
+  rt <- rt1/(1-rt1)
+      
+  tkksh ~ dgamma(0.001, 0.001)
+  tkkrt1 ~ dbeta(1,1)
+  tkkrt <- tkkrt1/(1-tkkrt1)
+
+  k ~ dgamma(0.001, 0.001)
+  intercept ~ dgamma(0.001, 0.001)
+  
+  # MODEL 
+  for(n in 1:N){	# run through pop
+    
+    for (t in 1:Ti){ # run through timepoints
+      
+      Status[n,t] ~ dbern(prev)
+      
+      lambda[n,t] <- tKK[n,Status[n,t]+1,t] # this needs to go here because each time the r loop reiterates it will replace this value
+        
+        for( r in 1:R){  # run through repeat measures
+          KK[n,t,r] ~ dnegbin(rtnb/(lambda[n,t]+rtnb),rtnb) # generating the data with noise and then sampling from the dataset with the gamma sh/rt parameters?
+      } # end or r loop
+
+    
+      tKK[n,1,t] <- 0                                                                                                                                                                                                                                                                                               
+      tKK[n,2,t] ~ dgamma(tkksh, tkkrt)
+      
+      prob[n,t,1] ~ dnorm(0,3.093451)
+      prob[n,t,2] ~ dnorm(4 / (1 + exp(-k*(tKK[n,2,t]-intercept))),3.093451)
+       
+      CCA[n,t] ~ dround(prob[n,t,Status[n,t]+1],0)
+      
+      } # end timestep 1 t loop
+    } #N
+  
+  #inits# .RNG.seed, .RNG.name, Status, sigma, prob   
+  #data# N, Ti, R, KK, CCA
+  #monitor#  prev, rtnb, k, intercept, sh, rt, Status, CCA, tKK
+}"
+  
+  # Run model #
+  Results <- run.jags(m, burnin=5000, sample=10000, n.chains=2, jags.refresh = 1, method = 'parallel',
+                      plots = F, silent.jags = F)
+  return(Results)
+}
+
+
+
+#### Data manipulation functions ####
+
+
+#### Data loading ####
+
+#### Kato Katz Data ####
+
+loadKKdata <- function(nameFile){
+  dt <- read.csv(nameFile) # read data file
+  
+  # name different weeks
+  dt$dateN <- NA
+  dt$dateN[which(dt$date=="25/09/2017" | dt$date=="26/09/2017" | dt$date=="27/09/2017"
+                 | dt$date=="28/09/2017" | dt$date=="29/09/2017" | dt$date=="02/10/2017")] <- "Pre-T"
+  
+  dt$dateN[which(dt$date=="23/10/2017" | dt$date=="24/10/2017" | dt$date=="25/10/2017"
+                 | dt$date=="26/10/2017" | dt$date=="27/10/2017")] <- "3 weeks"
+  
+  dt$dateN[which(dt$date=="04/12/2017" | dt$date=="05/12/2017")] <- "9 weeks"
+  
+  dt$dateN[which(dt$date=="01/03/2018" | dt$date=="05/03/2018" | dt$date=="06/03/2018"
+                 | dt$date=="07/03/2018" | dt$date=="08/03/2018" | dt$date=="09/03/2018")] <- "6 months"
+  
+  dates <- c(na.omit(unique(dt$dateN)))
+  # set as array dimension (number children, number timesteps, number max of repeats)
+  children <- unique(dt$child_id)
+  KK <- array(NA,dim = c(length(children),4,6))
+  for (i in 1:length(children)){
+    KK[i,,] <- getKKChild(children[i],dt,dates)
+  }
+  return(KK)
+}
+
+getKKChild <- function(ID,dt,dates){
+  dts <- subset(dt,child_id==ID,select = c(child_id,Sm_A,Sm_B,dateN))
+  KKChild <- matrix(NA,nrow=length(dates),ncol=6)
+  for (i in 1:length(dates)){
+    KKChild[i,] <- getKKChildWeek(dates[i],dts)
+  }
+  return(KKChild)
+}
+
+getKKChildWeek <- function(weekN,dts){
+  repeatsKK <- c(dts$Sm_A[which(dts$dateN==weekN)],dts$Sm_B[which(dts$dateN==weekN)])
+  length(repeatsKK)<-6 #set max number of repeats!
+  return(repeatsKK)
+}
+
+getKKChildIDs <- function(nameFile){
+  dt <- read.csv(nameFile)
+  return(sort(unique(dt$child_id)))
+}
+
+#### Normal CCA Data ####
+
+loadCCAdata <- function(nameFile){
+  dt <- read.csv(nameFile, stringsAsFactors = F)
+  dt$Poppy[which(dt$Poppy == "I*" | dt$Poppy=="-")]<-NA
+  
+  # restructure for categorical
+  dt$Poppy[which(dt$Poppy=="T")] <- 1
+  dt$Poppy[which(dt$Poppy==3)]<-4
+  dt$Poppy[which(dt$Poppy==2)]<-3
+  dt$Poppy[which(dt$Poppy==1)]<-2
+  dt$Poppy[which(dt$Poppy==0.5)]<-1
+  
+  # name different weeks
+  dt$dateN <- NA
+  dt$dateN[which(dt$date_on_tube=="25/09/2017" | dt$date_on_tube=="26/09/2017" | dt$date_on_tube=="27/09/2017"
+                 | dt$date_on_tube=="28/09/2017" | dt$date_on_tube=="29/09/2017" | dt$date_on_tube=="02/10/2017")] <- "Pre-T"
+  
+  dt$dateN[which(dt$date_on_tube=="23/10/2017" | dt$date_on_tube=="24/10/2017" | dt$date_on_tube=="25/10/2017"
+                 | dt$date_on_tube=="26/10/2017" | dt$date_on_tube=="27/10/2017")] <- "3 weeks"
+  
+  dt$dateN[which(dt$date_on_tube=="04/12/2017" | dt$date_on_tube=="05/12/2017")] <- "9 weeks"
+  
+  dt$dateN[which(dt$date_on_tube=="01/03/2018" | dt$date_on_tube=="05/03/2018" | dt$date_on_tube=="06/03/2018"
+                 | dt$date_on_tube=="07/03/2018" | dt$date_on_tube=="08/03/2018" | dt$date_on_tube=="09/03/2018")] <- "6 months"
+  
+  dates <- c(na.omit(unique(dt$dateN)))
+  # set as array dimension (number children, number timesteps)
+  children <- unique(dt$CID)
+  CCA <- array(NA,dim = c(length(children),4))
+  for (i in 1:length(children)){
+    CCA[i,] <- getCCAChild(children[i],dt,dates)
+  }
+  return(CCA)
+} # end of  function 
+
+# function for getting CCA child
+getCCAChild <- function(ID,dt,dates){
+  dts <- subset(dt,CID==ID,select = c(CID,Poppy,dateN))
+  CCAChild <- rep(NA,length(dates))
+  for (i in 1:length(dates)){
+    if (!is.null(which(dts$dateN==dates[i]))){
+      CCAChild[i] <- ifelse(is.null(dts$Poppy[which(dts$dateN==dates[i])]),NA,as.numeric(dts$Poppy[which(dts$dateN==dates[i])]))
+    }
+  }
+  return(CCAChild)
+}
+
+# function for getting CCA child ID
+getCCAChildIDs <- function(nameFile){
+  dt <- read.csv(nameFile)
+  return(sort(unique(dt$CID)))
+}
+
+#### GScore data ####
+
+loadgscoredata <- function(nameFile){
+  #restructure so that it is the right scoring so that a
+  # score of 1 which is negative, is given a score of 0.
+  dt <- read.csv(nameFile, stringsAsFactors = F)
+  
+  dt$gscore <- dt$gscore-1
+  
+  # name different weeks
+  dt$dateN <- NA
+  dt$dateN[which(dt$date_on_tube=="25/09/2017" | dt$date_on_tube=="26/09/2017" | dt$date_on_tube=="27/09/2017"
+                 | dt$date_on_tube=="28/09/2017" | dt$date_on_tube=="29/09/2017" | dt$date_on_tube=="02/10/2017")] <- "Pre-T"
+  
+  dt$dateN[which(dt$date_on_tube=="23/10/2017" | dt$date_on_tube=="24/10/2017" | dt$date_on_tube=="25/10/2017"
+                 | dt$date_on_tube=="26/10/2017" | dt$date_on_tube=="27/10/2017")] <- "3 weeks"
+  
+  dt$dateN[which(dt$date_on_tube=="04/12/2017" | dt$date_on_tube=="05/12/2017")] <- "9 weeks"
+  
+  dt$dateN[which(dt$date_on_tube=="01/03/2018" | dt$date_on_tube=="05/03/2018" | dt$date_on_tube=="06/03/2018"
+                 | dt$date_on_tube=="07/03/2018" | dt$date_on_tube=="08/03/2018" | dt$date_on_tube=="09/03/2018")] <- "6 months"
+  
+  dates <- c(na.omit(unique(dt$dateN)))
+  # set as array dimension (number children, number timesteps)
+  children <- unique(dt$CID)
+  CCAgscore <- array(NA,dim = c(length(children),4))
+  for (i in 1:length(children)){
+    CCAgscore[i,] <- getCCAChildgscore(children[i],dt,dates)
+  }
+  return(CCAgscore)
+} # end of  function 
+
+# function for getting CCA child
+getCCAChildgscore <- function(ID,dt,dates){
+  dts <- subset(dt,CID==ID,select = c(CID,gscore,dateN))
+  CCAChildgscore <- rep(NA,length(dates))
+  for (i in 1:length(dates)){
+    if (!is.null(which(dts$dateN==dates[i]))){
+      CCAChildgscore[i] <- ifelse(is.null(dts$gscore[which(dts$dateN==dates[i])]),NA,as.numeric(dts$gscore[which(dts$dateN==dates[i])]))
+    }
+  }
+  return(CCAChildgscore)
+}
+
+# function for getting CCA child ID
+getCCAChildIDsgscore <- function(nameFile){
+  dt <- read.csv(nameFile)
+  return(sort(unique(dt$CID)))
+}
+
+#### sort model output into time steps ####
+
+time.steps <- function(model.output){
+  require(reshape2)
+  t1 <- as.data.frame(model.output[,1:210])
+  t2 <- as.data.frame(model.output[,211:420])
+  t3 <- as.data.frame(model.output[,421:630])
+  t4 <- as.data.frame(model.output[,631:840])
+  
+  t1[nrow(t1)+1,] <- colSums(t1)
+  t1[nrow(t1),] <- t1[nrow(t1),]/20000
+  
+  t1.means <- as.data.frame(t1[20001,1:210])
+  
+  t1.means$time <-as.factor("Baseline")
+  
+  T1 <- melt(t1.means)
+  
+  t2[nrow(t2)+1,] <- colSums(t2)
+  t2[nrow(t2),] <- t2[nrow(t2),]/20000
+  
+  t2.means <- as.data.frame(t2[20001,1:210])
+  
+  t2.means$time <-as.factor("ThreeWeeks")
+  
+  T2 <- melt(t2.means)
+  
+  t3[nrow(t3)+1,] <- colSums(t3)
+  t3[nrow(t3),] <- t3[nrow(t3),]/20000
+  
+  t3.means <- as.data.frame(t3[20001,1:210])
+  
+  t3.means$time <- as.factor("NineWeeks")
+  
+  T3 <- melt(t3.means)
+  
+  t4[nrow(t4)+1,] <- colSums(t4)
+  t4[nrow(t4),] <- t4[nrow(t4),]/20000
+  
+  t4.means <- as.data.frame(t4[20001,1:210])
+  
+  t4.means$time <- as.factor("SixMonths")
+  
+  T4 <- melt(t4.means)
+  
+  props <- rbind(T1, T2, T3, T4)
+  
+  props$time <- as.factor(props$time)
+  props$variable <- as.factor(props$variable)
+  
+  return(props)
+}
+
+
+
+
